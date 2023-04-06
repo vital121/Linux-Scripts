5a376caca09e5e94f1bdcd069149bccbe5b9ce5c
vital121 <123862035+vital121@users.noreply.github.com>
2023-04-06 03:03:01 +0100

    Create nodejsnvm.sh

---
+++ nodejsnvm.sh
@@ -0,0 +1,11 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+source ~/.bashrc
+nvm --version
+nvm list-remote
+echo "to install v19"
+echo "nvm install v19"
+echo "currrent version"
+node --version
+nvm ls
+#nvm use 16.14.2
+#nvm alias default 16.14.2
